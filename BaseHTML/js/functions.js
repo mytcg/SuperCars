222094bb6eb554991d354873c2ad837eead2acf0
Shaheed Samsodien <shaheed.samsodien@saatchisynergize.co.za>
2014-09-08 22:34:20 +0200

    renewed with bootstrap and scrapped jquery mobile

---
+++ BaseHTML/js/functions.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function() {
+
+    
+
+});
+
+function genericAjax(script, resultDiv, formID, js, waitForResult){
+
+    var wait = (waitForResult=='true') ? false : true;
+
+    var ajax = jQuery.ajax({
+        async: wait,
+        type: "POST",
+        url: script,
+        data : jQuery('#' + formID).serialize(),
+        success: function(data) {
+
+            if (jQuery('#' + resultDiv)) {
+
+                //$('#' + resultDiv).html(data).show();
+                jQuery('#' + resultDiv).html(data);
+
+                if (js && js!=undefined) {
+                    eval(''+js);
+                }
+                activatejQueryPlugins();
+            }
+        }
+    });
+}
