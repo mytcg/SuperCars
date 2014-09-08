f56634d4d8d37735d0aa46b04884e47ce3aac9d7
Shaheed Samsodien <shaheed.samsodien@saatchisynergize.co.za>
2014-09-09 01:30:46 +0200

    added dashboard html

--- BaseHTML/js/functions.js
+++ BaseHTML/js/functions.js
@@ -23,8 +23,44 @@ function genericAjax(script, resultDiv, formID, js, waitForResult){
                 if (js && js!=undefined) {
                     eval(''+js);
                 }
-                activatejQueryPlugins();
             }
         }
     });
+}
+
+function doLogin () {
+
+    var ajax = jQuery.ajax({
+        type: "POST",
+        crossDomain: true,
+        url: 'http://topcarcards.co.za/?request=categories&PHP_AUTH_PW='+$('#password').val()+'&PHP_AUTH_USER='+$('#username').val(),
+        data : '',
+        success: function(data) {
+
+                var result = xmlToArray(data);
+                if (result['RESULT']=='true') {
+                    window.location = 'dashboard.html';
+                }
+        }
+    });
+}
+
+function xmlToArray (xml) {
+
+    var thisArray = new Array();
+    if($(xml).children().length > 0){
+        $(xml).children().each(function(){
+            if($(xml).find(this.nodeName).children().length > 0){
+
+                //If it has children recursively get the inner array
+                var NextNode = $(xml).find(this.nodeName);
+                thisArray[this.nodeName] = xmlToArray(NextNode);
+
+            } else {
+                //If not then store the next value to the current array
+                thisArray[this.nodeName] = $(xml).find(this.nodeName).text();
+            }
+        });
+    }
+    return thisArray;
 }
