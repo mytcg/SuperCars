f1def0928b7666543d710fcd8a9e9127c50dc60f
Shaheed Samsodien <shaheed.samsodien@saatchisynergize.co.za>
2014-09-11 09:46:12 +0200

    decks complete

--- BaseHTML/js/functions.js
+++ BaseHTML/js/functions.js
@@ -300,12 +300,16 @@ function newDeck (name) {
     var ajax = jQuery.ajax({
         type: "POST",
         crossDomain: true,
-        url: 'http://topcarcards.co.za/?request=purchaseproduct&product_id='+product_id+appendToken,
+        url: 'http://topcarcards.co.za/?request=createdeck&deck_name='+name+appendToken,
         data : '',
         success: function(data) {
 
                 eval('var result='+data);
-                window.location('grid-template.html?name='+name+'&section=');
+                if (result['result']) {
+                    window.location='grid-template.html?section=decks';
+                } else {
+                    alert(result['content']);
+                }
         }
     });
 }
